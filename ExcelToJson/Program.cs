67fcb8febb114eb4444c69c27f691e304132d558
Edwin <sleepyrainyday@naver.com>
2020-02-16 19:57:35 +0900

    기본 프로그램이 일단 돌아가게 수정

--- ExcelToJson/Program.cs
+++ ExcelToJson/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using ExcelToJson.ExcelControl;
 using ExcelToJson.JsonConverter;
 
@@ -9,9 +11,40 @@ namespace ExcelToJson
     {
         static void Main(string[] args)
         {
-            using (var fileStream = new FileStream("mockupData.xlsx", FileMode.Open, FileAccess.Read))
+            ConvertExcelToJson("mockupData.xlsx", ".");
+        }
+
+        static void ConvertExcelToJson(string excelFilePath, string resultDir)
+        {
+            using (var excelFile = new FileStream(excelFilePath, FileMode.Open, FileAccess.Read))
             {
+                var excelLoader = new ExcelLoader(excelFile);
+
+                while (excelLoader.HasSheetToRead())
+                {
+                    var excelSheet = excelLoader.LoadNextSheet();
+
+                    var sheetFileName = $"{GetNameFromPath(excelFile.Name)}.{excelSheet.Name}.json";
+                    using (var jsonFile = new FileStream($"{resultDir}/{sheetFileName}", FileMode.Create, FileAccess.Write))
+                    {
+                        using (var streamWriter = new StreamWriter(jsonFile))
+                        {
+                            var collectionToJsonConverter = new CollectionToJsonConverter(excelSheet.Properties);
+
+                            streamWriter.Write(collectionToJsonConverter.CreateJSONLiteral(excelSheet.Sheet));
+                        }
+                    }
+                }
             }
+        }
+
+        static string GetNameFromPath(string path)
+        {
+            var regEx = new Regex(@"\\([a-zA-z0-9]+)\.xlsx$");
+
+            var matches = regEx.Matches(path);
+
+            return matches.Last().Groups[1].Value;
         }
     }
 }
