0f0f15ac9546c4e3f0bae3aa2308fa9f9085b9f2
Edwin <sleepyrainyday@naver.com>
2020-02-16 18:03:20 +0900

    Add UnitTest Project

---
+++ ExcelToJson/ExcelLoader/ExcelLoader.cs
@@ -0,0 +1,34 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExcelToJson.ExcelLoader
+{
+    class ExcelLoader
+    {
+        readonly IWorkbook excelWorkbook;
+        int currentSheetNumber;
+        int maxSheetNumber => excelWorkbook.NumberOfSheets;
+
+        public ExcelLoader(FileStream fileStream)
+        {
+            excelWorkbook = new HSSFWorkbook(fileStream);
+            currentSheetNumber = 0;
+        }
+
+        public string ReadNextSheet()
+        {
+            if(currentSheetNumber < maxSheetNumber)
+            {
+                var sheet = excelWorkbook.GetSheetAt(currentSheetNumber);
+
+                ++currentSheetNumber;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ExcelToJson/ExcelLoader/ExcelSheetLoader.cs
@@ -0,0 +1,36 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelToJson.ExcelLoader
+{
+    class ExcelSheetLoader
+    {
+        readonly ISheet sheet;
+        readonly int rowStart;
+        readonly int rowEnd;
+        readonly int maxColumnWidth;
+
+        public ExcelSheetLoader(ISheet sheet)
+        {
+            this.sheet = sheet;
+
+            rowStart = sheet.FirstRowNum;
+            rowEnd = sheet.LastRowNum;
+
+            maxColumnWidth = 0;
+
+            for (var i = rowStart; i <= rowEnd; i++)
+            {
+                var currentColumn = sheet.GetColumnWidth(i);
+                maxColumnWidth = Math.Max(maxColumnWidth, currentColumn);
+            }
+        }
+
+        string ReadRow(IRow row)
+        {
+
+        }
+    }
+}
